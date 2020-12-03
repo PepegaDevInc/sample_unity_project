3a242acac3dd29aa1eca7bca7214ac49146bea52
Miron Klimentenko <himynameismiron@mail.ru>
2020-12-03 03:31:29 +0300

    RequestContentField

--- Assets/RequestScript.cs
+++ Assets/RequestScript.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RequestScript : MonoBehaviour
 {
-    public GameObject TextField;
+    public GameObject ResponseStatusField;
     public Button GetResponceButton;
+    public GameObject ResponseContentField;
 
     void Start()
     {
@@ -21,6 +25,21 @@ public class RequestScript : MonoBehaviour
         WebRequest request = WebRequest.Create("https://httpbin.org/anything");
         request.Credentials = CredentialCache.DefaultCredentials;
         WebResponse response = request.GetResponse();
-        TextField.GetComponent<Text>().text = ((HttpWebResponse)response).StatusDescription;
+        //Fetching status
+        ResponseStatusField.GetComponent<Text>().text = "Status: " + ((HttpWebResponse)response).StatusDescription;
+        //Fetching responce body using Stream
+        Stream receiveStream = response.GetResponseStream();
+        Encoding encode = Encoding.GetEncoding("utf-8");
+        // Pipes the stream to a higher level stream reader with the required encoding format.
+        StreamReader readStream = new StreamReader(receiveStream, encode);
+        ResponseContentField.GetComponent<Text>().text = "";
+        while (readStream.Peek() >= 0)
+        {
+            string line = readStream.ReadLine();
+            ResponseContentField.GetComponent<Text>().text = ResponseContentField.GetComponent<Text>().text + line + "\n";
+        }
+        ResponseContentField.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+        response.Close();
+        readStream.Close();
     }
 }
